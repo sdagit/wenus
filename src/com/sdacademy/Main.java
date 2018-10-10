29750c688700c93d4e225f5551e926354742bd05
Karina Wilk desktop <sda_git@protonmail.com>
2018-10-10 16:10:54 +0200

    Szkielet aplikacji

---
+++ src/com/sdacademy/Main.java
@@ -0,0 +1,18 @@
+package com.sdacademy;
+
+import org.omg.CORBA.Environment;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("To jest prosty kalkulator! Aby \r\n"
+        + "- obliczyć pierwiastek => naciśnij 1 \n"
+        + "- obliczyć potęgę liczby => naciśnij 2 \n"
+        + "- obliczyć sinus kąta => naciśnij 3 \n" );
+
+        //TODO Odczytaj liczbę wprowadzoną przez użytkownika
+        //TODO: jeśli wprowadzona liczba jest inna niż 1,2 lub 3 - zakończ aplikację.
+        //TODO: Jeśli liczba jest równa 1, 2, lub 3 - wywołaj konkretną metodę (którą musisz wcześniej napisać.
+    }
+}
